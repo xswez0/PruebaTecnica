c374ed183684e5964283e52d39b2e3175f38ec72
Eduardo Javier <e.jav.diaz@outlook.es>
2023-03-01 22:38:00 -0500

    PruebaTecnica

---
+++ Backend/PruebaTecnicaData/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Internal;
+using PruebaTecnicaData.Models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PruebaTecnicaData
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DataContext() : base() { }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=PruebaTecnica;Trusted_Connection=True;");
+            }
+        }
+    }
+}
---
+++ Backend/PruebaTecnicaData/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace PruebaTecnicaData.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Area { get; set; }
+        public string Cargo { get; set; }
+    }
+}
